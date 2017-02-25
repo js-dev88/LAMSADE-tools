9dc241743b9635fb6f31e161a9d5511a56cf3e8c
Javier Martínez <edoreld@gmail.com>
2017-02-25 12:46:41 +0100

    added test class that use PDFBox to generate blank pdf

---
+++ src/com/github/lamsadetools/TestGround.java
@@ -0,0 +1,15 @@
+package com.github.lamsadetools;
+
+import java.io.IOException;
+
+import org.apache.pdfbox.examples.pdmodel.CreateBlankPDF;
+
+public class TestGround {
+
+	public static void main(String[] args) throws IOException {
+		
+		new CreateBlankPDF("test");
+
+	}
+
+}
