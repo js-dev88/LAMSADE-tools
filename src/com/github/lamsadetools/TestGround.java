d53610bd83dd4c172414f4014588ba4b05ba7eb0
Javier Martínez <edoreld@gmail.com>
2017-02-25 13:41:15 +0100

    merged pdf create & add image classes

--- src/com/github/lamsadetools/TestGround.java
+++ src/com/github/lamsadetools/TestGround.java
@@ -1,15 +1,17 @@
 package com.github.lamsadetools;
 
 import java.io.IOException;
-
-import org.apache.pdfbox.examples.pdmodel.CreateBlankPDF;
+import org.apache.pdfbox.examples.pdmodel.PDFManipulator;
 
 public class TestGround {
 
 	public static void main(String[] args) throws IOException {
 		
-		new CreateBlankPDF("test");
-
+		PDFManipulator pm = new PDFManipulator();
+		pm.createBlankPDF("test");
+		pm.addImageToPdf("test.pdf", "img/logo-dauphine.jpg", "test2.pdf");
+	
+        
 	}
 
 }
