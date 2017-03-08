9b362f76bfdac87f27c1df08e8d3ff9183b15f60
LAntoine <antoine.lepers@dauphine.eu>
2017-03-08 12:00:38 +0100

    added function readFromDatabase

--- src/com/github/lamsadetools/TestGround.java
+++ src/com/github/lamsadetools/TestGround.java
@@ -12,13 +12,15 @@ public class TestGround {
 	public static void main(String[] args) throws Exception {
 
 		ODTGenerator g = new ODTGenerator();
-		
+
 		String dateFormat = "dd/MM/yy";
 		DateFormat format = new SimpleDateFormat(dateFormat);
 		format.setLenient(false);
-		Conference conf = new Conference("url","Antoine s conf", format.parse("10/03/2017"), format.parse("11/03/2017"));
+		Conference conf = new Conference("url", "Antoine s conf", format.parse("10/03/2017"),
+				format.parse("11/03/2017"));
 
 		Conference.insertInDatabase(conf);
+		Conference.getFromDatabase(1);
 
 		// PapierAEnTeteGenerator pm = new PapierAEnTeteGenerator();
 		//
