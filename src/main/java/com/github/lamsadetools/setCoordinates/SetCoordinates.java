70dfc68d457e9b6b622a753c769a5c52f1811cb4
LAntoine <antoine.lepers@dauphine.eu>
2017-04-05 23:00:23 +0200

    Create a fonctionnal function fillPapierEnTete in SetCoordinates

--- src/main/java/com/github/lamsadetools/setCoordinates/SetCoordinates.java
+++ src/main/java/com/github/lamsadetools/setCoordinates/SetCoordinates.java
@@ -1,12 +1,19 @@
 package com.github.lamsadetools.setCoordinates;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.channels.FileChannel;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
@@ -35,12 +42,67 @@ public class SetCoordinates {
 		}
 	}
 
-	public static void main(String[] args) throws Exception {
-		String source = "papier_en_tete.docx";
-		String destination = "papier_en_tete_CloneTest.docx";
+	public static void fillPapierEnTete(UserDetails user) throws Exception {
+		Path path = FileSystems.getDefault().getPath("");
+		System.out.println("The File will be saved in: " + path.toAbsolutePath());
 
+		// define source and target
+		ClassLoader classLoader = SetCoordinates.class.getClassLoader();
+		String source = classLoader.getResource("com/github/lamsadetools/setCoordinates/papier_a_en_tete.fodt")
+				.getPath();
+		String target = path.toAbsolutePath() + "/papier_a_en_tete_Clone.fodt";
+
+		// open source and copy it in target only changing what needs to be
+		// changed
+		FileOutputStream fos = new FileOutputStream(target);
+		try (PrintStream printStream = new PrintStream(fos)) {
+			try (BufferedReader brSource = new BufferedReader(new FileReader(source))) {
+				// copy lines until 36763 because we want to keep them
+				for (int i = 0; i < 36762; i++) {
+					String lineSource = brSource.readLine();
+					printStream.println(lineSource);
+				}
+
+				// change the next 2 lines with user's informations
+				// 1st line
+				String lineSource = brSource.readLine();
+				String regex = "Nom";
+				Pattern p = Pattern.compile(regex);
+				Matcher m = p.matcher(lineSource);
+				lineSource = m.replaceFirst(user.getName());
+				regex = "Prenom";
+				p = Pattern.compile(regex);
+				m = p.matcher(lineSource);
+				lineSource = m.replaceFirst(user.getFirstName());
+				regex = "Tel";
+				p = Pattern.compile(regex);
+				m = p.matcher(lineSource);
+				lineSource = m.replaceFirst(user.getNumber());
+				printStream.println(lineSource);
+
+				// 2nd line
+				lineSource = brSource.readLine();
+				regex = "Fonction";
+				p = Pattern.compile(regex);
+				m = p.matcher(lineSource);
+				lineSource = m.replaceFirst(user.getFunction());
+				regex = "E-mail";
+				p = Pattern.compile(regex);
+				m = p.matcher(lineSource);
+				lineSource = m.replaceFirst(user.getEmail());
+				printStream.println(lineSource);
+
+				// copy lines until the end because we want to keep them
+				while ((lineSource = brSource.readLine()) != null) {
+					printStream.println(lineSource);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
 		UserDetails user = promptUserDetails();
-		setDetails(source, destination, user);
+		fillPapierEnTete(user);
 		System.out.println("Done");
 
 	}
@@ -94,22 +156,28 @@ public class SetCoordinates {
 	 * @throws IOException
 	 * @throws InvalidFormatException
 	 */
-	public static void setDetails(String source, String destination, UserDetails user)
-			throws IOException, InvalidFormatException {
+	public static void setDetails(UserDetails user) throws IOException, InvalidFormatException {
+		String source = "com/github/lamsadetools/setCoordinates/papier_en_tete.docx";
+		String destination = "com/github/lamsadetools/setCoordinates/papier_en_tete_CloneTest.docx";
 
-		try (XWPFDocument doc = new XWPFDocument(OPCPackage.open(source))) {
+		ClassLoader classLoader = SetCoordinates.class.getClassLoader();
+
+		File sourceFile = new File(classLoader.getResource(source).getFile());
+		File destinationFile = new File(classLoader.getResource(destination).getFile());
+		copy(sourceFile, destinationFile);
+
+		try (XWPFDocument doc = new XWPFDocument(OPCPackage.open(sourceFile))) {
 			XWPFHeaderFooterPolicy policy = doc.getHeaderFooterPolicy();
-			File sourceFile = new File(source);
-			File destinationFile = new File(destination);
-			copy(sourceFile, destinationFile);
 
 			for (XWPFParagraph p : policy.getFirstPageHeader().getParagraphs()) {
 				List<XWPFRun> runs = p.getRuns();
 				if (runs != null) {
 					for (XWPFRun r : runs) {
 						String text = r.getText(0);
+						System.out.println("text:" + text);
 
 						if ((text != null) && text.contains("Prenom")) {
+							System.out.println("contains prenom");
 							text = text.replace("Prenom", user.getFirstName());
 
 							r.setText(text, 0);
@@ -147,7 +215,7 @@ public class SetCoordinates {
 					}
 				}
 			}
-			try (FileOutputStream fos = new FileOutputStream(destination)) {
+			try (FileOutputStream fos = new FileOutputStream(destinationFile)) {
 				doc.write(fos);
 			}
 		}
