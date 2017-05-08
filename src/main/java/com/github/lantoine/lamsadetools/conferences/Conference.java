a182a41c9900dba5cceddca3cc6f8d03ab5acf84
js-dev88 <js.dev88@gmail.com>
2017-05-08 17:59:21 +0200

    Unique name identifier updated

---
+++ src/main/java/com/github/lantoine/lamsadetools/conferences/Conference.java
@@ -0,0 +1,232 @@
+package com.github.lantoine.lamsadetools.conferences;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
+import net.fortuna.ical4j.data.CalendarOutputter;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Dur;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.ValidationException;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.property.CalScale;
+import net.fortuna.ical4j.model.property.Description;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.Version;
+
+/**
+ * This class enable us to store some of the informations that a teacher may
+ * want to store about his future conferences. it contains functions to store
+ * informations in a database and a user interface to create, search, view, edit
+ * and delete the conferences.
+ *
+ * @author lantoine
+ *
+ */
+public class Conference {
+
+	public static final String DATE_FORMAT = "dd/MM/yyyy";
+
+	private LocalDate end_date;
+
+	private double entry_fee;
+
+	private int id;
+
+	private LocalDate start_date;
+
+	private String title;
+
+	private String url;
+
+	public Conference(int id, String title, String url, LocalDate start_date, LocalDate end_date, double entry_fee) {
+		this.id = id;
+		this.url = url;
+		this.title = title;
+		this.start_date = start_date;
+		this.end_date = end_date;
+		this.entry_fee = entry_fee;
+	}
+
+	public Conference(String title, String url, LocalDate start_date, LocalDate end_date, double entry_fee) {
+		this(0, title, url, start_date, end_date, entry_fee);
+	}
+
+	@Override
+	/**
+	 * Compare the conference to obj by comparing all attributes except id
+	 */
+	public boolean equals(Object obj) {
+		if (obj instanceof Conference) {
+			Conference conference2 = (Conference) obj;
+			System.out.println(toString());
+			System.out.println(conference2.toString());
+			if (title.equals(conference2.title) && url.equals(conference2.url)
+					&& start_date.equals(conference2.start_date) && end_date.equals(conference2.end_date)
+					&& (entry_fee == conference2.entry_fee)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public LocalDate getEnd_date() {
+		return end_date;
+	}
+
+	public double getEntry_fee() {
+		return entry_fee;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public LocalDate getStart_date() {
+		return start_date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * Overriding a required method
+	 */
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	public void setEnd_date(LocalDate end_date) {
+		this.end_date = end_date;
+	}
+
+	public void setEntry_fee(double entry_fee) {
+		this.entry_fee = entry_fee;
+	}
+
+	public void setStart_date(LocalDate start_date) {
+		this.start_date = start_date;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return "Conference [id=" + id + "title=" + title + ", url=" + url + ", start_date=" + start_date + ",end_date="
+				+ end_date + ", entry_fee=" + entry_fee + "]";
+	}
+
+	/**
+	 * Generates a vcal file with the conference details with this object's data
+	 *
+	 * @param filename:
+	 *            the path including the name of the file to be generated
+	 * @throws IOException
+	 * @throws ValidationException
+	 * @throws ParserException
+	 *
+	 * @author Javier Martínez
+	 */
+	public void generateCalendarFile(String filename) throws IOException, ValidationException, ParserException {
+		String calFile = filename;
+
+		// start time
+		java.util.Calendar startCal = java.util.Calendar.getInstance();
+
+		startCal.setTime(java.sql.Date.valueOf(getStart_date()));
+		// end time
+		java.util.Calendar endCal = java.util.Calendar.getInstance();
+		endCal.setTime(java.sql.Date.valueOf(getEnd_date()));
+
+		String subject = "Conference";
+		String description = "A conference with a fee of " + getEntry_fee();
+		String hostEmail = "";
+
+		// Creating a new calendar
+		net.fortuna.ical4j.model.Calendar calendar = new net.fortuna.ical4j.model.Calendar();
+		calendar.getProperties().add(new ProdId("-//Ben Fortuna//iCal4j 1.0//EN"));
+		calendar.getProperties().add(Version.VERSION_2_0);
+		calendar.getProperties().add(CalScale.GREGORIAN);
+
+		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyyMMdd'T'hhmmss'Z'");
+		String strDate = sdFormat.format(startCal.getTime());
+
+		net.fortuna.ical4j.model.Date startDt = null;
+		try {
+			startDt = new net.fortuna.ical4j.model.Date(strDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		long diff = endCal.getTimeInMillis() - startCal.getTimeInMillis();
+		int min = (int) (diff / (1000 * 60));
+
+		Dur dur = new Dur(0, 0, min, 0);
+
+		// Creating a meeting event
+		VEvent meeting = new VEvent(startDt, dur, subject);
+
+		// This is where you would add a location if there was one
+		// meeting.getProperties().add(new Location(location));
+		meeting.getProperties().add(new Description());
+
+		try {
+			meeting.getProperties().getProperty(Property.DESCRIPTION).setValue(description);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		// try {
+		// meeting.getProperties().add(new Organizer(null, "MAILTO:" +
+		// hostEmail));
+		// } catch (URISyntaxException e) {
+		// e.printStackTrace();
+		// }
+
+		calendar.getComponents().add(meeting);
+
+		FileOutputStream fout = null;
+
+		try {
+			fout = new FileOutputStream(calFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		CalendarOutputter outputter = new CalendarOutputter();
+		outputter.setValidating(false);
+
+		try {
+			outputter.output(calendar, fout);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ValidationException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(meeting);
+
+	}
+
+}
---
+++ src/main/java/com/github/lantoine/lamsadetools/graphical_interface/Win1.java
@@ -0,0 +1,108 @@
+package com.github.lantoine.lamsadetools.graphical_interface;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class Win1 {
+
+	    public Win1(Display display) {
+
+	        initUI(display);
+	    }
+
+	    private void initUI(Display display) {
+	        
+	    	
+	    	//Display display = new Display();
+	    	Shell shell = new Shell(display, SWT.SHELL_TRIM | SWT.CENTER);
+	    	
+	      
+
+	        Color col = new Color(display, 100, 220, 150);
+	        shell.setBackground(col);
+	        col.dispose();
+	        
+	        GridLayout layout = new GridLayout(2, false);
+	        shell.setLayout(layout);
+	        
+	        Label lbl1 = new Label(shell, SWT.NONE);
+	        Composite composite  = new Composite(shell,SWT.FILL);
+	        lbl1.setLayoutData(composite);           
+	        
+	        Color col1 = new Color(display, 255, 155, 10);
+	        lbl1.setBackground(col1);
+	        col1.dispose();        
+	            
+	      
+	        
+	        Text txtTest = new Text(composite, SWT.NONE);
+	        txtTest.setText("Testing");
+	       
+
+	        Text txtMoreTests = new Text(composite, SWT.NONE);
+	        txtMoreTests.setText("Another test");
+	        
+	        Button button = new Button(composite, SWT.PUSH);
+	        button.setText("Press Me");
+	        button.addListener(SWT.Selection, new Listener() {
+	            public void handleEvent(Event e) {
+	              switch (e.type) {
+	              case SWT.Selection:
+	                System.out.println("Button pressed");
+	                break;
+	              }
+	            }
+	          });
+	        
+	      
+	        
+	        Label lbl3 = new Label(shell, SWT.NONE);
+	        GridData gd3 = new GridData(SWT.FILL, SWT.FILL, true, true);
+	        gd3.widthHint = 1000;
+	        gd3.heightHint = 500;
+	        gd3.horizontalSpan = 2;
+	        lbl3.setLayoutData(gd3);           
+	        
+	        Color col3 = new Color(display, 100, 205, 200);
+	        lbl3.setBackground(col3);
+	        col3.dispose();        
+
+	        shell.setText("Grid");
+	        shell.pack();
+	        shell.open();
+	        
+	        while (!shell.isDisposed()) {
+	            if (!display.readAndDispatch())
+	                display.sleep();
+	        }
+	    }
+
+	    @SuppressWarnings("unused")
+	    public static void main(String[] args) {
+	        
+	        Display display = new Display();
+	        Win1 ex = new Win1(display);
+	        display.dispose();
+	    }
+	
+
+}
