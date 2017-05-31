65dbd38efe602328600d0420d69e2ea58fffb3c5
LAntoine <antoine.lepers@dauphine.eu>
2017-05-31 13:21:25 +0200

    finished autofilling of NAME and grade

--- src/main/java/com/github/lantoine/lamsadetools/setCoordinates/PRESGenerator.java
+++ src/main/java/com/github/lantoine/lamsadetools/setCoordinates/PRESGenerator.java
@@ -1,33 +1,26 @@
 package com.github.lantoine.lamsadetools.setCoordinates;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintStream;
-import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.odftoolkit.odfdom.doc.OdfTextDocument;
-import org.odftoolkit.odfdom.incubator.search.TextNavigation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.lantoine.lamsadetools.conferences.IO;
-import com.github.lantoine.lamsadetools.yearbookInfos.GetInfosFromYearbook;
 
 public class PRESGenerator
 
 {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(PRESGenerator.class);
+
 	/**
 	 * Create a PRES document
 	 *
@@ -36,73 +29,73 @@ public class PRESGenerator
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-//		OdfTextDocument textdoc = OdfTextDocument
-//				.loadDocument("com/github/lamsadetools/setCoordinates/Premier_test_template.odt");
-//
-//		/* Creation of search for every word needed to be replaced */
-//		TextNavigation searchN;
-//		TextNavigation searchP;
-//		TextNavigation searchG;
-//		TextNavigation searchE;
-//		TextNavigation searchT;
-//
-//		searchN = new TextNavigation("NOMR", textdoc);
-//		searchP = new TextNavigation("PRENOMR", textdoc);
-//		searchG = new TextNavigation("GRADER", textdoc);
-//		searchE = new TextNavigation("TEMPSPARTIELLER", textdoc);
-//		searchT = new TextNavigation("TEMPSPLEINR", textdoc);
-//
-//		/* Verify all the word and replace them by the value */
-//		while (searchN.hasNext()) {
-//
-//			// TextSelection item = (TextSelection) searchN.nextSelection();
-//
-//			// item.replaceWith("Test");
-//
-//		}
-//		while (searchP.hasNext()) {
-//
-//			// TextSelection item = (TextSelection) searchP.nextSelection();
-//
-//			// item.replaceWith("Test");
-//
-//		}
-//		while (searchG.hasNext()) {
-//
-//			// TextSelection item = (TextSelection) searchG.nextSelection();
-//
-//			// item.replaceWith("Test");
-//
-//		}
-//		while (searchE.hasNext()) {
-//
-//			// TextSelection item = (TextSelection) searchE.nextSelection();
-//
-//			// item.replaceWith("Test");
-//
-//		}
-//		while (searchT.hasNext()) {
-//
-//			// TextSelection item = (TextSelection) searchT.nextSelection();
-//
-//			// item.replaceWith("Test");
-//
-//		}
+		// OdfTextDocument textdoc = OdfTextDocument
+		// .loadDocument("com/github/lamsadetools/setCoordinates/Premier_test_template.odt");
+		//
+		// /* Creation of search for every word needed to be replaced */
+		// TextNavigation searchN;
+		// TextNavigation searchP;
+		// TextNavigation searchG;
+		// TextNavigation searchE;
+		// TextNavigation searchT;
+		//
+		// searchN = new TextNavigation("NOMR", textdoc);
+		// searchP = new TextNavigation("PRENOMR", textdoc);
+		// searchG = new TextNavigation("GRADER", textdoc);
+		// searchE = new TextNavigation("TEMPSPARTIELLER", textdoc);
+		// searchT = new TextNavigation("TEMPSPLEINR", textdoc);
+		//
+		// /* Verify all the word and replace them by the value */
+		// while (searchN.hasNext()) {
+		//
+		// // TextSelection item = (TextSelection) searchN.nextSelection();
+		//
+		// // item.replaceWith("Test");
+		//
+		// }
+		// while (searchP.hasNext()) {
+		//
+		// // TextSelection item = (TextSelection) searchP.nextSelection();
+		//
+		// // item.replaceWith("Test");
+		//
+		// }
+		// while (searchG.hasNext()) {
+		//
+		// // TextSelection item = (TextSelection) searchG.nextSelection();
+		//
+		// // item.replaceWith("Test");
+		//
+		// }
+		// while (searchE.hasNext()) {
+		//
+		// // TextSelection item = (TextSelection) searchE.nextSelection();
+		//
+		// // item.replaceWith("Test");
+		//
+		// }
+		// while (searchT.hasNext()) {
+		//
+		// // TextSelection item = (TextSelection) searchT.nextSelection();
+		//
+		// // item.replaceWith("Test");
+		//
+		// }
 
 		UserDetails user = promptUserDetails();
 		fillPRES(user);
 		logger.info("PRES generated");
-		
+
 	}
-	
+
 	public static void fillPRES(UserDetails user) throws Exception {
 		Path path = FileSystems.getDefault().getPath("");
 		System.out.println("The File will be saved in: " + path.toAbsolutePath());
 
 		// define source and target
 		ClassLoader classLoader = PRESGenerator.class.getClassLoader();
-		String source = classLoader.getResource("com/github/lantoine/lamsadetools/PRESGenerator/Premier_test_template.fodt")
-				.getPath();
+		String source = classLoader
+				.getResource("com/github/lantoine/lamsadetools/PRESGenerator/Premier_test_template.fodt").getPath();
 		String target = path.toAbsolutePath() + "/Premier_test_template_Clone.fodt";
 
 		// open source and copy it in target only changing what needs to be
@@ -111,35 +104,44 @@ public class PRESGenerator
 		try (PrintStream printStream = new PrintStream(fos)) {
 			try (BufferedReader brSource = new BufferedReader(new FileReader(source))) {
 				// copy lines until 2338 because we want to keep them
-				for (int i = 0; i < 2338; i++) {
+				for (int i = 0; i < 2323; i++) {
 					String lineSource = brSource.readLine();
 					printStream.println(lineSource);
 				}
 
 				// change the next lines with user's informations
 
+				// Name line
 				String lineSource = brSource.readLine();
 				String regex = "NOMR";
 				Pattern p = Pattern.compile(regex);
 				Matcher m = p.matcher(lineSource);
 				lineSource = m.replaceFirst(user.getName());
-				
+				printStream.println(lineSource);
+
+				// Firstname line
+				lineSource = brSource.readLine();
 				regex = "PRENOMR";
 				p = Pattern.compile(regex);
 				m = p.matcher(lineSource);
 				lineSource = m.replaceFirst(user.getFirstName());
-				
+				printStream.println(lineSource);
+
+				// Grade line
+				lineSource = brSource.readLine();
 				regex = "GRADER";
 				p = Pattern.compile(regex);
 				m = p.matcher(lineSource);
 				lineSource = m.replaceFirst(user.getGroup());
-				
+				printStream.println(lineSource);
+
+				// Time line
 				lineSource = brSource.readLine();
 				regex = "TPSPLR";
 				p = Pattern.compile(regex);
 				m = p.matcher(lineSource);
 				lineSource = m.replaceFirst(user.getFunction());
-
+				printStream.println(lineSource);
 
 				// copy lines until the end because we want to keep them
 				while ((lineSource = brSource.readLine()) != null) {
@@ -148,19 +150,19 @@ public class PRESGenerator
 			}
 		}
 	}
-	
+
 	public static UserDetails promptUserDetails() {
 		String tableauQuestion[] = { "Your name ?", "Your first name?", "Your grade?", "Full time job ?" };
 		IO io = new IO();
 		io.scanner = new Scanner(System.in);
 		UserDetails user = new UserDetails();
-		for (int i = 0; i <= (tableauQuestion.length - 1); i++) {
+		for (int i = 0; i <= tableauQuestion.length - 1; i++) {
 
 			System.out.println(tableauQuestion[i]);
 
 			if (tableauQuestion[i].equals("Your grade?")) {
 
-				user.setGroup (io.scanner.nextLine());
+				user.setGroup(io.scanner.nextLine());
 
 			} else if (tableauQuestion[i].equals("Your name ?")) {
 
