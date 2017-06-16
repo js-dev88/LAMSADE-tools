07c30e578da0eb8cc4c8c253a186947c881eee4a
LAntoine <antoine.lepers@dauphine.eu>
2017-06-16 13:19:37 +0200

    Finished PreferenceWindow

--- src/main/java/com/github/lantoine/lamsadetools/graphical_interface/PreferencesWindow.java
+++ src/main/java/com/github/lantoine/lamsadetools/graphical_interface/PreferencesWindow.java
@@ -1,11 +1,73 @@
 package com.github.lantoine.lamsadetools.graphical_interface;
 
+import java.nio.file.FileSystems;
+import java.util.prefs.Preferences;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PreferencesWindow {
+	private static final Logger LOGGER = LoggerFactory.getLogger(PreferencesWindow.class);
+	private static String str_working_dir;
+	private static Text text_working_dir;
+	private static Label lbl_directory;
+
 	static void open(Display display) {
-		Shell shellPreference = new Shell(display);
-		shellPreference.open();
+		// This will define a node in which the preferences can be stored
+		Preferences prefs = Preferences.userRoot().node("graphical_prefs :");
+		Shell shell = new Shell(display);
+		RowLayout layout = new RowLayout();
+		layout.wrap = false;
+		shell.setLayout(layout);
+
+		str_working_dir = prefs.get("working_dir", FileSystems.getDefault().getPath("").toAbsolutePath().toString());
+
+		lbl_directory = new Label(shell, SWT.PUSH);
+		lbl_directory.setText("default output directory");
+
+		text_working_dir = new Text(shell, SWT.PUSH);
+		text_working_dir.setText(str_working_dir);
+
+		Button btnSaveOrdreMission = new Button(shell, SWT.NONE);
+		btnSaveOrdreMission.setText("Browse");
+		btnSaveOrdreMission.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				DirectoryDialog fileDialog = new DirectoryDialog(shell, SWT.OPEN);
+				LOGGER.info("Opening the file dialog for user to choose where he wants to store his files");
+				String dialogResult = fileDialog.open();
+				LOGGER.info("Location chosen: " + dialogResult);
+
+				if (dialogResult == null) {
+					LOGGER.info("User closed the directory dialog");
+				} else {
+					prefs.put("working_dir", dialogResult);// store the absolute
+															// path
+					text_working_dir.setText(str_working_dir);
+					LOGGER.debug("directory set to" + str_working_dir);
+				}
+			}
+		});
+		shell.pack();
+		shell.open();
+	}
+
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	public static void main() {
+		System.setProperty("SWT_GTK3", "0");
+		Display display = new Display();
+		open(display);
 	}
 }
