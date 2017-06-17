3510c8bbc50153c8eb275dbda45d3e621b0e12c2
js-dev88 <js.dev88@gmail.com>
2017-06-17 16:38:44 +0200

    OMtitle

--- src/main/java/com/github/lantoine/lamsadetools/missionOrder/generateMissionOrder.java
+++ src/main/java/com/github/lantoine/lamsadetools/missionOrder/generateMissionOrder.java
@@ -1,7 +1,9 @@
 package com.github.lantoine.lamsadetools.missionOrder;
 
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.FileSystems;
+import java.sql.SQLException;
 
 import org.odftoolkit.simple.SpreadsheetDocument;
 import org.odftoolkit.simple.table.Cell;
@@ -9,37 +11,47 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.lantoine.lamsadetools.conferences.Conference;
+import com.github.lantoine.lamsadetools.keyring.EmailPassword;
+import com.github.lantoine.lamsadetools.keyring.EmailPasswordDatabase;
 import com.github.lantoine.lamsadetools.setCoordinates.UserDetails;
+import com.github.lantoine.lamsadetools.utils.Util;
 import com.github.lantoine.lamsadetools.yearbookInfos.GetInfosFromYearbook;
+import com.github.windpapi4j.InitializationFailedException;
+import com.github.windpapi4j.WinAPICallFailedException;
 import com.sun.star.lang.IllegalArgumentException;
+import com.sun.star.uno.Exception;
 
 import ch.qos.logback.classic.Level;
 
 public class generateMissionOrder {
 	@SuppressWarnings("unused")
 	private static final Logger LOGGER = LoggerFactory.getLogger(generateMissionOrder.class);
-
-	private static String target = FileSystems.getDefault().getPath("").toAbsolutePath() + "/ordre_de_mission_test.ods";
+    
+	private String target;
 
 	public String getTarget() {
 		return target;
 	}
+	public void setTarget(String title) {
+		URL path = generateMissionOrder.class.getResource("ordre_de_mission.ods");
+		this.target = path.toString() + "/" + title;
+	}
 
-	
-	public generateMissionOrder(){
+	public generateMissionOrder() {
 
 	}
 
 	/**
-	 * @param userDetails 
+	 * @param userDetails
 	 * @param conference
-	 * Use the userDetails and conference to fill the Spreadsheet
-	 * @throws Exception
+	 *            Use the userDetails and conference to fill the Spreadsheet
+	 * @throws java.lang.Exception 
 	 */
-	public void generateSpreadsheetDocument(UserDetails userDetails, Conference conference) throws Exception {
+	public void generateSpreadsheetDocument(UserDetails userDetails, Conference conference) throws java.lang.Exception {
 
 		try (InputStream inputStream = generateMissionOrder.class.getResourceAsStream("ordre_de_mission.ods");
 				SpreadsheetDocument spreadsheetDoc = SpreadsheetDocument.loadDocument(inputStream)) {
+			URL path = generateMissionOrder.class.getResource("ordre_de_mission.ods");
 			// Name
 			Cell nameCell = spreadsheetDoc.getSheetByName("Feuil1").getCellByPosition("F8");
 			nameCell.setStringValue(userDetails.getName());
@@ -65,27 +77,26 @@ public class generateMissionOrder {
 
 			Cell cityCountryArrivalReturnTrip = spreadsheetDoc.getSheetByName("Feuil1").getCellByPosition("T37");
 			cityCountryArrivalReturnTrip.setStringValue(userDetails.getCity() + " ," + userDetails.getCountry());
-			
+
 			// Date trip
-			
-			
+
 			// write the field mission about the date of mission
 
-				Cell dateDeparture = spreadsheetDoc.getSheetByName("Feuil1").getCellByPosition("M25");
-				dateDeparture.setStringValue(conference.getStart_date().toString());
+			Cell dateDeparture = spreadsheetDoc.getSheetByName("Feuil1").getCellByPosition("M25");
+			dateDeparture.setStringValue(conference.getStart_date().toString());
 
-				Cell dateArrival = spreadsheetDoc.getSheetByName("Feuil1").getCellByPosition("Z25");
-				dateArrival.setStringValue(conference.getEnd_date().toString());
+			Cell dateArrival = spreadsheetDoc.getSheetByName("Feuil1").getCellByPosition("Z25");
+			dateArrival.setStringValue(conference.getEnd_date().toString());
 
-			
-
-			spreadsheetDoc.save("ordre_de_mission_test.ods");
-			inputStream.close();
-			//spreadsheetDoc.close();
-
-		
-
+			spreadsheetDoc.save(path.toString() + "/" + generateTitle(conference));
 		}
+	}
+
+	public String generateTitle(Conference conference) {
+		String title;
+		title = "OM_" + conference.getCity() + "_" + conference.getStart_date().toString();
+		return title;
+
 	}
 
 }
