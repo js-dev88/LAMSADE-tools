b2f13878dfbd4e232a800108446f1943611ba314
js-dev88 <js.dev88@gmail.com>
2017-06-18 09:17:09 +0200

    Test for Exception in sendEmail - Util is repaired

--- src/test/java/com/github/lantoine/lamsadetools/utils/TestUtil.java
+++ src/test/java/com/github/lantoine/lamsadetools/utils/TestUtil.java
@@ -1,16 +1,14 @@
 package com.github.lantoine.lamsadetools.utils;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class TestUtil {
 	
-	@Test
+	@Test(expected=IllegalStateException.class)
 	public void testSendEmailStringString() {
-		assertEquals(11, 11);
-		/*assertEquals("Sending an email didn't work", 0,
-				Util.sendEmail("1234567890abcdefghijklmnopqrstuvwxy@mailinator.com"));*/
+		
+		Util.sendEmail("1234567890abcdefghijklmnopqrstuvwxy@mailinator.com","test");
+		
 	}
 
 	// @Test
